41fe5f2c10760f6e754e64e3578e7c432693f223
Alex Schuessler <AS@schuess.com>
2019-09-03 11:14:29 -0500

    Added functionality with withRouter to dynamically link page routing to UI.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+ <h1>HATS PAGE</h1>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/"  component = {HomePage}/>
+        <Route exact path="/hats"  component = {HatsPage}/> 
+      </Switch> 
     </div>
   );
 }
