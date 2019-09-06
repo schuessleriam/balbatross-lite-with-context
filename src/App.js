766b59aaa60a54d94f3d69cfaae06ef2c6875655
Alex Schuessler <AS@schuess.com>
2019-09-06 09:15:55 -0500

    added redirect functionality to homepage after successful sign ins. Modified styling for homepage and header component. centralized user action types as variables in seperate file.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -44,15 +44,20 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/"  component = {HomePage}/>
           <Route exact path="/shop"  component = {ShopPage}/>
-          <Route exact path="/signin"  component = {SignInAndSignUp}/> 
+          <Route exact path="/signin"  render={() => this.props.currentUser ? <Redirect to='/'/> : <SignInAndSignUp/>}/> 
         </Switch> 
       </div>
     );    
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
+
+
 const MapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, MapDispatchToProps)(App);
+export default connect(mapStateToProps, MapDispatchToProps)(App);
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,10 +1,12 @@
+import {userActionTypes} from './user.action.types';
+
 const INITIAL_STATE = {
     currentUser: null
 }
 
 const userReducer = (state = INITIAL_STATE, action) =>{
     switch(action.type){
-        case 'SET_CURRENT_USER':
+        case userActionTypes.setCurrentUser :
             return {
                 ...state,
                 currentUser: action.payload
