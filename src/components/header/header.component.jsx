7d9dd18414a7dd1281316c6ae59349547cf8663f
Alex Schuessler <AS@schuess.com>
2019-09-10 15:29:46 -0500

    added checkout page and checkut item component. Updated cart dropdown component to navigate to checkout. Updated header and app components to use selectors.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,22 @@
+// import base react libraries 
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import firebase authorization library
 import { auth } from './../../firebase/firebase.utils';
+// import redux modules and project selectors
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartHidden } from "./../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "./../../redux/user/user.selectors";
+// import logo and styling
 import { ReactComponent as Logo } from "./../../assets/balbatross.svg";
-import CartIcon from "./../cart-icon/cart-icon.component";
-
 import "./header.styles.scss";
+// import project components
+import CartIcon from "./../cart-icon/cart-icon.component";
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
+
+// -- Header functional component -- //
 const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
@@ -31,9 +40,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser,
-    hidden: state.cart.hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
