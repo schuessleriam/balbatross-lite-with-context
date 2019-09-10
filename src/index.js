9e9a4b9841b2965990267f4ac5a2673062dbe85c
Alex Schuessler <AS@schuess.com>
2019-09-10 18:23:56 -0500

    added redux-persist library and implemented with store to allow for refresh with cart reducer.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Store from './redux/store';
+import { store, persistor } from './redux/store';
 import  { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
-    <Provider store={Store}>
+    <Provider store={store}>
         <BrowserRouter> 
-            <App /> 
+            <PersistGate persistor={persistor}>
+                <App /> 
+            </PersistGate>
         </BrowserRouter>     
     </Provider>,
     document.getElementById('root'));
